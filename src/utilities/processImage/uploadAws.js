a591c7fbbafa25780e1c39905f4da61e081404f5
Akash Chhetri <akash.chhetri1@gmail.com>
2020-10-17 20:39:59 +1100

    test done for awsurl

--- src/utilities/processImage/uploadAws.js
+++ src/utilities/processImage/uploadAws.js
@@ -2,7 +2,6 @@ import { uploadFile } from 'react-s3';
 import CONFIG from '../../config/config';
 
 const uploadAws = async (file) => {
-	console.log(file, 'check this');
 	try {
 		const resp = await uploadFile(file, CONFIG);
 		return resp.location;
